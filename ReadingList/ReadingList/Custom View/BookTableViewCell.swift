d58d7123a31aed4a4ef58c855cd1233002534ad1
Alex Rhodes <anrhodes5738@gmail.com>
2019-08-20 18:55:51 -0800

    Storyboard Setup

---
+++ ReadingList/ReadingList/Custom View/BookTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BookTableViewCell.swift
+//  ReadingList
+//
+//  Created by Alex Rhodes on 8/20/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    
+}
---
+++ ReadingList/ReadingList/Model & Model Controller/BookController.swift
@@ -0,0 +1,9 @@
+//
+//  BookController.swift
+//  ReadingList
+//
+//  Created by Alex Rhodes on 8/20/19.
+//  Copyright © 2019 Alex Rhodes. All rights reserved.
+//
+
+import Foundation
